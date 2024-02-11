495b479280eacf020ce18cc80f465045e24962ef
JuanBeltran09 <beltranrodriguezjuan9@gmail.com>
2024-02-10 20:32:08 -0500

    Verify user login using phone auth credential

--- app/src/main/java/com/example/diemtest/auth/LoginActivity.kt
+++ app/src/main/java/com/example/diemtest/auth/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.diemtest.MainActivity
 import com.example.diemtest.R
 import com.example.diemtest.databinding.ActivityLoginBinding
@@ -16,6 +17,10 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.concurrent.TimeUnit
 
 class LoginActivity : AppCompatActivity() {
@@ -24,11 +29,15 @@ class LoginActivity : AppCompatActivity() {
     val auth = FirebaseAuth.getInstance()
     private var verificationId: String? = null
 
+    private lateinit var dialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dialog = AlertDialog.Builder(this).setView(R.layout.loading_layout).setCancelable(false).create()
 
         binding.sendOtp.setOnClickListener{
             if(binding.userNumber.text!!.isEmpty()){
@@ -50,6 +59,7 @@ class LoginActivity : AppCompatActivity() {
     private fun verifyOtp(otp: String) {
         //binding.sendOtp.showLoadingButton()
 
+        dialog.show()
         val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
 
         signInWithPhoneAuthCredential(credential)
@@ -59,6 +69,7 @@ class LoginActivity : AppCompatActivity() {
 
         //binding.sendOtp.showLoadingButton()
 
+        dialog.show()
         val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
@@ -77,6 +88,7 @@ class LoginActivity : AppCompatActivity() {
             ) {
                 this@LoginActivity.verificationId = verificationId
 
+                dialog.dismiss()
                 //binding.sendOtp.showNormalButton()
 
                 binding.numberLayout.visibility = GONE
@@ -99,12 +111,36 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 //binding.sendOtp.showNormalButton()
                 if (task.isSuccessful) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+
+                    checkUserExist(binding.userNumber.text.toString())
+
                 } else {
+                    dialog.dismiss()
                     Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
 
                 }
             }
+    }
+
+    private fun checkUserExist(number: String) {
+
+        FirebaseDatabase.getInstance().getReference("users").child(number)
+            .addValueEventListener(object  : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+                    dialog.dismiss()
+                    Toast.makeText(this@LoginActivity, p0.message, Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    if(p0.exists()){
+                        dialog.dismiss()
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finish()
+                    } else{
+                        startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+                    }
+                }
+            })
+
     }
 }
