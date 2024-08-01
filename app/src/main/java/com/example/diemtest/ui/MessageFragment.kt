d5ef290a07f72d50951cc7aacc4ebf9048796ffb
JuanBeltran09 <beltranrodriguezjuan9@gmail.com>
2024-08-01 14:08:11 -0500

    chat app using firebase kotlin in android with firebase realtime database

--- app/src/main/java/com/example/diemtest/ui/MessageFragment.kt
+++ app/src/main/java/com/example/diemtest/ui/MessageFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.diemtest.R
 import com.example.diemtest.adapter.MessageUserAdapter
 import com.example.diemtest.databinding.FragmentMessageBinding
 import com.example.diemtest.ui.DatingFragment.Companion.list
+import com.example.diemtest.utils.Config
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MessageFragment : Fragment() {
 
@@ -21,9 +28,38 @@ class MessageFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMessageBinding.inflate(layoutInflater)
 
-        binding.recyclerView.adapter = MessageUserAdapter(requireContext(), list!!)
+        getData()
 
         return binding.root
+    }
+
+    private fun getData() {
+        Config.showDialog(requireContext())
+        var list = arrayListOf<String>()
+        var newList = arrayListOf<String>()
+        val currentId = FirebaseAuth.getInstance().currentUser!!.phoneNumber
+        FirebaseDatabase.getInstance().getReference("chats").addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (data in snapshot.children){
+
+                    if (data.key!!.contains(currentId!!)){
+                        list.add(data.key!!.replace(currentId!!,""))
+                        newList.add(data.key!!)
+                    }
+
+                }
+
+                binding.recyclerView.adapter = MessageUserAdapter(requireContext(), list, newList)
+
+                Config.hideDialog()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(),error.message,Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
 }
